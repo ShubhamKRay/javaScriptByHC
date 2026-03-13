fe2dc5f66ec52a00b2bf441537b3ccc1469168ee
ShubhamKRay <shubhairmen@gmail.com>
2026-03-13 23:43:53 +0530

    this and arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,65 @@
+const user = {
+  username: "Shubham",
+  price: 400,
+
+  welcomeMessage: function () {
+    console.log(`${this.username} , welcome to our website!`);
+    console.log(this);
+  },
+};
+// user.welcomeMessage()
+// user.username = "Satyarth";
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function shubh(){
+//     console.log(this);
+// }
+
+// function shubh(){
+//     let username = "Shubham";
+//     console.log(this.username);
+// }
+// shubh();
+
+// const shubh = function(){
+//     let username = "Shubham";
+//     console.log(this.username);
+// }
+// shubh();
+
+
+
+const shubh = () => {
+  let username = "Shubham";
+  console.log(this);
+};
+// shubh();
+
+
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2;
+// }
+
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => (num1 + num2);
+
+
+const addTwo = (num1, num2) => ({username: "Shubham"});
+
+
+// console.log(addTwo(3,4));
+
+
+
+const myArray = [1, 2, 3, 4, 5];
+
+myArray.forEach(function (num) {
+  console.log(num);
+});
+
+myArray.forEach((num) => console.log(num));
